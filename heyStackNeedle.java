ce125d8147a82402f22cd15aecd9331fda79a2f7
NoraLeo <38717394+NoraLeo@users.noreply.github.com>
2023-03-14 22:05:21 +0800

    Create heyStackNeedle.java

---
+++ heyStackNeedle.java
@@ -0,0 +1,10 @@
+//my answer to 28. Find the Index of the First Occurrence in a String on LeetCode
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        //haystack is the main string
+        //needle is the substring in which we have to find its first occurence.
+        return (haystack.indexOf(needle));
+        
+    }
+}
